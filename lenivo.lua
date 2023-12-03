fe2a3a27bd6693344e9035c021a3cf08b12b8d6a
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-12-03 15:10:41 +0300

    Vendor Files
    ДА ХЗ

---
+++ lenivo.lua
@@ -0,0 +1,58 @@
+drawed = drawed or {}
+
+if not drawed[screen] then
+    local rx, ry = bootloader.initScreen(gpu, screen, 80, 25)
+    local sx, sy = 42, 10
+    local x, y = ((rx / 2) - (sx / 2)) + 1, ((ry / 2) - (sy / 2)) + 1
+    local depth = gpu.getDepth()
+
+    if depth ~= 1 then
+        gpu.setPaletteColor(0, 0x000000)
+        gpu.setPaletteColor(1, 0xff0000)
+        gpu.setPaletteColor(2, 0xffffff)
+    else
+        y = y - 1
+    end
+
+    gpu.setBackground(0x000000)
+    gpu.fill(1, 1, rx, ry, " ")
+
+    gpu.setBackground(0xff0000)
+    gpu.fill(x, y, sx, sy, " ")
+
+    if depth == 1 then
+        gpu.setForeground(0x000000)
+    else
+        gpu.setForeground(0xffffff)
+    end
+
+    gpu.set(x, y + 0, "                                          ")
+    gpu.set(x, y + 1, "  █     █████  █     █  █  █    █   ███   ")
+    gpu.set(x, y + 2, "  █     █      ██    █     █    █  █   █  ")
+    gpu.set(x, y + 3, "  █     █      █ █   █  █   █  █   █   █  ")
+    gpu.set(x, y + 4, "  █     █      █  █  █  █   █  █   █   █  ")
+    gpu.set(x, y + 5, "  █     █████  █  █  █  █   █  █   █   █  ")
+    gpu.set(x, y + 6, "  █     █      █   █ █  █   █  █   █   █  ")
+    gpu.set(x, y + 7, "  █     █      █    ██  █   █  █   █   █  ")
+    gpu.set(x, y + 8, "  ████  █████  █     █  █    ██     ███   ")
+    gpu.set(x, y + 9, "                                       TM ")
+    gpu.set(x, y +10, "                                          ")
+else
+    gpu.bind(screen, false)
+    local rx, ry = gpu.getResolution()
+    gpu.setBackground(0x000000)
+    gpu.fill(1, ry - 2, rx, 1, " ")
+end
+
+if text then
+    while text:sub(#text, #text) == "." do
+        text = text:sub(1, #text - 1)
+    end
+
+    local rx, ry = gpu.getResolution()
+    gpu.setBackground(0x000000)
+    gpu.setForeground(0xffffff)
+    gpu.set(math.floor(((rx / 2) - (unicode.len(text) / 2)) + 0.5) + 1, ry - 2, text or "")
+end
+
+drawed[screen] = true
---
+++ toDev.lua
@@ -0,0 +1,175 @@
+local data = [[
+local installdata = {data={branch="dev",mode="full"}}
+
+local function initScreen(gpu, screen)
+    if gpu.getScreen() ~= screen then
+        gpu.bind(screen, false)
+    end
+    gpu.setDepth(1)
+    gpu.setDepth(gpu.maxDepth())
+    gpu.setResolution(50, 16)
+    gpu.setBackground(0)
+    gpu.setForeground(0xFFFFFF)
+    gpu.fill(1, 1, 50, 16, " ")
+end
+
+local function centerPrint(gpu, text, y)
+    local rx, ry = gpu.getResolution()
+    gpu.set((math.floor(rx / 2) - (#text / 2)) + 1, y, text)
+end
+
+local screensInited
+local function printState(num)
+    local str = "working with updates: " .. tostring(math.floor((num * 100) + 0.5)) .. "%"
+    local gpu = component.proxy(component.list("gpu")() or "")
+    if gpu then
+        for screen in component.list("screen") do
+            if not screensInited then
+                initScreen(gpu, screen)
+
+                if gpu.getDepth() > 1 then
+                    gpu.setPaletteColor(0, 0x5bb9f0)
+                    gpu.setPaletteColor(1, 0xffffff)
+
+                    gpu.setBackground(0, true)
+                    gpu.setForeground(1, true)
+                else
+                    gpu.setBackground(0xFFFFFF)
+                    gpu.setForeground(0x000000)
+                end
+            elseif gpu.getScreen() ~= screen then
+                gpu.bind(screen, false)
+            end
+
+            local rx, ry = gpu.getResolution()
+            gpu.fill(1, 1, rx, ry, " ")
+            centerPrint(gpu, str, math.floor(ry / 2))
+            centerPrint(gpu, "please do not turn off the device!", ry - 1)
+        end
+        screensInited = true
+    end
+end
+
+printState(0)
+
+--------------------------------
+
+local internet = component.proxy(component.list("internet")() or error("no internet card found", 0))
+local proxy = component.proxy(computer.getBootAddress())
+
+local function getInternetFile(url)
+    local handle, data, result, reason = internet.request(url), ""
+    if handle then
+        while true do
+            result, reason = handle.read(math.huge) 
+            if result then
+                data = data .. result
+            else
+                handle.close()
+                
+                if reason then
+                    return nil, reason
+                else
+                    return data
+                end
+            end
+        end
+    else
+        return nil, "unvalid address"
+    end
+end
+
+local function split(str, sep)
+    local parts, count, i = {}, 1, 1
+    while 1 do
+        if i > #str then break end
+        local char = str:sub(i, #sep + (i - 1))
+        if not parts[count] then parts[count] = "" end
+        if char == sep then
+            count = count + 1
+            i = i + #sep
+        else
+            parts[count] = parts[count] .. str:sub(i, i)
+            i = i + 1
+        end
+    end
+    if str:sub(#str - (#sep - 1), #str) == sep then table.insert(parts, "") end
+    return parts
+end
+
+local function segments(path)
+    local parts = {}
+    for part in path:gmatch("[^\\/]+") do
+        local current, up = part:find("^%.?%.$")
+        if current then
+            if up == 2 then
+                table.remove(parts)
+            end
+        else
+            table.insert(parts, part)
+        end
+    end
+    return parts
+end
+
+local function canonical(path)
+    local result = table.concat(segments(path), "/")
+    if unicode.sub(path, 1, 1) == "/" then
+        return "/" .. result
+    else
+        return result
+    end
+end
+
+local function fs_path(path)
+    local parts = segments(path)
+    local result = table.concat(parts, "/", 1, #parts - 1) .. "/"
+    if unicode.sub(path, 1, 1) == "/" and unicode.sub(result, 1, 1) ~= "/" then
+        return canonical("/" .. result)
+    else
+        return canonical(result)
+    end
+end
+
+local function saveFile(path, data)
+    proxy.makeDirectory(fs_path(path))
+    local file = proxy.open(path, "wb")
+    proxy.write(file, data)
+    proxy.close(file)
+end
+
+local function installUrl(urlPart, state2)
+    local filelist = split(assert(getInternetFile(urlPart .. "/installer/filelist.txt")), "\n")
+    for i, v in ipairs(filelist) do
+        if v ~= "" then
+            local filedata = assert(getInternetFile(urlPart .. v))
+
+            if i % 10 == 0 then
+                printState((((i - 1) / (#filelist - 1)) / 2) + (state2 and 0.5 or 0))
+            end
+
+            saveFile(v, filedata)
+        end
+    end
+end
+
+--удаляем старую систему во избежании конфликта версий
+proxy.remove("/system")
+
+--сначала ставим liked а только потом ядро, чтобы не перезаписывать init.lua раньше времени. чтобы если обновления оборветься то система не окирпичилась
+installUrl("https://raw.githubusercontent.com/igorkll/liked/" .. installdata.data.branch)
+installUrl("https://raw.githubusercontent.com/igorkll/likeOS/" .. installdata.data.branch, true)
+
+--востанавливаем содержимое sysdata
+for name, content in pairs(installdata.data) do
+    saveFile("/system/sysdata/" .. name, content)
+end
+
+--удаляем этот файл
+proxy.remove("/likeOS_startup.lua")
+
+--перезагружаем устройтсво
+computer.shutdown("fast")
+]]
+require("filesystem").writeFile("/init.lua", data)
+require("computer").shutdown("fast")
